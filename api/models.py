08727a014302f841f95bcf692495db9e97e4415d
Kevin130902 <kevinnalter@gmail.com>
2024-09-20 16:50:27 -0300

    Poucas mudanças

--- api/models.py
+++ api/models.py
@@ -4,3 +4,8 @@ class Produto(models.Model):
     codigoProduto = models.IntegerField(max_length=10)
     tituloProduto = models.CharField(max_length=255)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.TextField(max_length=500)
+    imagemProduto = models.CharField(max_length=255)
+    categoriaProduto = models.CharField(max_length=255)
+    classProduto = models.CharField(max_length=255)
+    exibirNome = models.BooleanField(default=True)
